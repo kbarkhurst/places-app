ffc211e75df9a23560c73bcb501207941e2a3202
Kelly Barkhurst <kellybarkhurst@gmail.com>
2021-03-29 17:16:36 -0400

    Finishes HW

---
+++ app/controllers/api/places_controller.rb
@@ -0,0 +1,45 @@
+class Api::PlacesController < ApplicationController
+  def index
+    @places = Place.all
+    render "index.json.jb"
+  end
+
+  def show
+    place_id = params[:id]
+    @place = Place.find_by(id: place_id)
+    render "show.json.jb"
+  end
+
+  def create
+    @place = Place.new(
+      name: params[:name],
+      address: params[:address],
+    )
+    if @place.save
+      render "show.json.jb"
+    else
+      render json: { errors: @place.errors.full_messages }, status: 406
+    end
+  end
+
+  def update
+    place_id = params[:id]
+    @place = Place.find_by(id: place_id)
+
+    @place.name = params[:name] || @place.name
+    @place.address = params[:address] || @place.address
+
+    if @place.save
+      render "show.json.jb"
+    else
+      render json: { errors: @place.errors.full_messages }, status: 406
+    end
+  end
+
+  def destroy
+    place_id = params[:id]
+    place = Place.find_by(id: place_id)
+    place.destroy
+    render json: { message: "Your location was successfully deleted" }
+  end
+end
